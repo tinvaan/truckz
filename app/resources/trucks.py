90b61b1c6109b75f6bbea353eb09c7f233598fc5
R. Harish Navnit <harishnavnit@gmail.com>
2016-09-14 22:18:46 +0530

    Experimenting with the directory structure a bit

---
+++ app/resources/trucks.py
@@ -0,0 +1,63 @@
+from flask_restful import Resource
+
+TRUCKS = {  }
+
+class Trucks(Resource):
+
+    def get(self):
+        return
+
+    def put(self):
+        return
+
+    def get_id(self):
+        return
+
+    def get_model(self):
+        return
+
+    def get_weight(self):
+        return
+
+    def get_volume(self):
+        return
+
+    def get_current_location(self):
+        return
+
+    def get_registration_number(self):
+        return
+
+    def set_id(self):
+        return
+
+    def set_model(self):
+        return
+
+    def set_weight(self):
+        return
+
+    def set_volume(self):
+        return
+
+    def set_current_location(self):
+        return
+
+    def set_registration_number(self):
+        return
+
+class TrucksList(Resource):
+    def get(self):
+        return
+
+    def put(self):
+        return
+
+    def get_trucks_list(self):
+        return
+
+    def add(Truck):
+        return
+
+    def remove(Truck):
+        return
