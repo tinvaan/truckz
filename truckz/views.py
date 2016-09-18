5ec892f773489a74699744492a9c77bfb3fd3779
R. Harish Navnit <harishnavnit@gmail.com>
2016-09-18 15:26:48 +0530

    Define and refactor views and fix url strings

--- truckz/views.py
+++ truckz/views.py
@@ -1,31 +1,75 @@
-from app import app
-from flask import render_template, url_for
+from truckz import app
+from truckz.app_db import get_database, init_database
+from flask import render_template, url_for, redirect, flash, session, request, abort, jsonify
 
 @app.route('/')
 def index():
-    render_template('login.html')
+    return render_template('index.html')
 
-@app.route('/trucks')
-def trucks_list():
-    return
+def login(user):
+    error = None
+    db = get_database()
+    init_database()
 
-#@app.route('/trucks/<str: id>')
-#def truck():
-#    return
+    if request.method == 'POST':
+        if user == 'owner':
+            rows = db.execute('select owner_auth_username and owner_auth_password from owners where owner_auth_username=? and owner_auth_password=?',
+                            [request.form['username'], request.form['password']])
+        elif user == 'customer':
+            rows = db.execute('select customer_auth_username, customer_auth_password from customers where customer_auth_username=(?) and customer_auth_password=(?)',
+                              [request.form['Username'], request.form['password']])
+        else:
+            abort(404, message={'User not found'})
+        if rows > 0:
+            session['logged_in'] = True
+            flash('Login successful')
+            return redirect(url_for('index'))
+        else:
+            error='Username or password not found in database'
+    return render_template('login.html', error=error)
 
-@app.route('/owners')
-def owners_list():
-    return
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Logged out')
+    return redirect(url_for('index'))
 
-#@app.route('/owners/<str: id>')
-#def owner():
-#    return
+@app.route('/trucks', defaults={'path':''})
+@app.route('/trucks/<path:path>')
+def show_trucks(path):
+    db = get_database()
+    init_database()
+    if path == '':
+        rows = db.execute('select * from trucks')
+    else:
+        rows = db.execute('select * from trucks where truck_id =(?)', path)
+    trucks = rows.fetchall()
+    return jsonify(trucks)
 
-@app.route('/customers')
-def customers_list():
-    return
+@app.route('/owners', defaults={'path':''}, methods=['POST', 'GET'])
+@app.route('/owners/<path:path>')
+def show_owners(path):
+    db = get_database()
+    init_database()
+    if path == '':
+        rows = db.execute('select * from owners')
+    elif path == 'login':
+        return login('owner')
+    else:
+        rows = db.execute('select * from owners where owner_id=(?)', path)
+    owners = rows.fetchall()
+    return jsonify(owners)
 
-#@app.route('/customers/<str: id>')
-#def customer():
-#    return
-
+@app.route('/customers', defaults={'path':''}, methods=['POST', 'GET'])
+@app.route('/customers/<path:path>')
+def show_customers(path):
+    db = get_database()
+    init_database()
+    if path == '':
+        rows = db.execute('select * from customers')
+    elif path == 'login':
+        return login('customer')
+    else:
+        rows = db.execute('select * from customers where customers_id=(?)', path)
+    customers = rows.fetchall()
+    return jsonify(customers)
